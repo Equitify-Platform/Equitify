ecd90178acad55e64d70bc111c13d47360d40c19
Ilya Kubariev <ilya.kubariev@redduck.io>
2022-10-26 13:58:03 +0300

    feat: ft metadata

--- frontend/src/contracts/ft.ts
+++ frontend/src/contracts/ft.ts
@@ -1,21 +1,22 @@
 import { Wallet } from "../near-wallet";
 
+interface FtContractMetadata {
+  name: string;
+  symbol: string;
+  decimals: string;
+}
+
 export class FungibleToken {
   constructor(
     private readonly contractId: string,
     private readonly wallet: Wallet
   ) {}
 
-  public async name(): Promise<string> {
-    return await this.wallet.view<string>(this.contractId, "ft_name");
-  }
-
-  public async symbol(): Promise<string> {
-    return await this.wallet.view<string>(this.contractId, "ft_symbol");
-  }
-
-  public async decimals(): Promise<string> {
-    return await this.wallet.view<string>(this.contractId, "ft_decimals");
+  public async metadata(): Promise<FtContractMetadata> {
+    return await this.wallet.view<FtContractMetadata>(
+      this.contractId,
+      "ft_metadata"
+    );
   }
 
   public async totalSupply(): Promise<string> {
