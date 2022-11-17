0f6eb563ccc9422f7f7cabd682601b3504e49a22
sowell-owen <karakulin.toshik@gmail.com>
2022-11-17 12:46:08 +0200

    feat: ido navigation mock

---
+++ frontend/src/components/IDONavigation/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+import styles from "./style.module.scss";
+
+const IDONavigation: FC = () => {
+  return <div>IDONavigation</div>;
+};
+
+export default IDONavigation;
