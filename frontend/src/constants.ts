0cf6da5914921cadbac915cc7f2a8de95d72117d
sowell-owen <karakulin.toshik@gmail.com>
2022-11-18 13:53:17 +0200

    chore: new ido projects

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -5,9 +5,9 @@ export const HELLO_NEAR_ADDRESS = "dev-1666191057769-59817066669753";
 export const STAKING_ADDRESS = "dev-1666191057769-59817066669753";
 
 export const LAUNCHPADS_ADDRESSES: string[] = [
-  "ido-launchpad-test17.launchpad-deployer.testnet",
-  "ido-launchpad-test27.launchpad-deployer.testnet",
   "ido-launchpad-test25.launchpad-deployer.testnet",
+  "ido-launchpad-test27.launchpad-deployer.testnet",
   "ido-launchpad-test29.launchpad-deployer.testnet",
   "ido-launchpad-test30.launchpad-deployer.testnet",
+  "ido-launchpad-test32.launchpad-deployer.testnet",
 ];
